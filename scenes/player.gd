d9f82b17e6aef75f648975121ff5856675d9e744
simplysoup <nickbode71@gmail.com>
2025-05-22 01:22:58 -0400

    now he move face

--- scenes/player.gd
+++ scenes/player.gd
@@ -12,19 +12,19 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_pressed("walk_right"):
 		velocity.x += 1
-		$AnimatedSprite2D.play("walk_right")
+		#$AnimatedSprite2D.play("walk_right")
 	if Input.is_action_pressed("walk_left"):
 		velocity.x -= 1
-		$AnimatedSprite2D.play("walk_left")
+		#$AnimatedSprite2D.play("walk_left")
 	if Input.is_action_pressed("walk_up"):
 		velocity.y -= 1
-		$AnimatedSprite2D.play("walk_up")
+		#$AnimatedSprite2D.play("walk_up")
 	if Input.is_action_pressed("walk_down"):
 		velocity.y += 1
-		$AnimatedSprite2D.play("walk_down")
+		#$AnimatedSprite2D.play("walk_down")
 		
 	if velocity.length() == 0:
-		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.play("idle")
 	
 	velocity = velocity.normalized() * speed
 	
